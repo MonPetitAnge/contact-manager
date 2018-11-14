c047ae36ea938007d1601d322af55c85ddaa1c02
Walid Mahmoud - Tchisuky <mwalid.mahmoud@gmail.com>
2018-11-15 00:15:18 +0100

    :sparkles: :newspaper: adding registration form
    SÃcuritÃ© avec hash bcyrpt + formulaire d'inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,18 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"login"},
+ *     message="Le login que vous avez entré est déjà utilisé !"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -20,13 +27,33 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Votre nom d'utilisateur doit faire au moins {{ limit }} caractères",
+     *      maxMessage = "Votre nom d'utilisateur ne peut pas dépasser les {{ limit }} caractères"
+     * )
      */
     private $login;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 8,
+     *      max = 50,
+     *      minMessage = "Votre mots de passe doit faire au moins {{ limit }} caractères",
+     *      maxMessage = "Votre nom d'utilisateur ne peut pas dépasser les {{ limit }} caractères"
+     * )
      */
     private $password;
+
+    /**
+     * @var Pour être sûr que lors de l'enregistrement l'utilisateur tape deux fois son mots de passe
+     * @Assert\EqualTo(propertyPath="password", message="Vous n'avez pas tapé le même mots de passe")
+     */
+    public $confirmPassword;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Contact", mappedBy="user", orphanRemoval=true)
@@ -96,5 +123,57 @@ class User
         }
 
         return $this;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return array('ROLE_USER');
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername(): string
+    {
+        return $this->getLogin();
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
